d899a7ba93d3cdbce42d378ed5de184802a1f190
kk4w4i <kintarokawai@gmail.com>
2024-08-18 03:29:41 +1000

    ui bugs

--- client/src/customComponents/Navbar.tsx
+++ client/src/customComponents/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar({combat, time}: NavbarProps) {
             {combat ? (
                 <div className="flex justify-between w-full border-b-[1px] border-neutral-200 font-inter px-10 py-5">
                     <h1 className="text-[1.5rem]">
-                        CodeDual.
+                        CodeDuel.
                     </h1>
                     <section className="flex gap-10 items-center">
                         <div className="flex items-center justify-center space-x-4">
@@ -30,7 +30,7 @@ export default function Navbar({combat, time}: NavbarProps) {
              ) : (
                 <div className="flex justify-center w-full font-inter px-10 py-5 mt-10">
                     <h1 className="text-[3rem]">
-                        CodeDual.
+                        CodeDuel.
                     </h1> 
                 </div> 
              )
