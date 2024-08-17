b3a19b62202d5d736df47074103cbf6b15359b36
Theodore Kim <tkim0476@gmail.com>
2024-08-17 11:58:23 +1000

    erm what the sigma

--- questions/readers/python_readers.py
+++ questions/readers/python_readers.py
@@ -18,28 +18,16 @@ from typing import List
 
 
 class Problems:
+    def lintconverter(stdin: str) -> List[int]:
+        nums = list(map(int, stdin.strip().split(',')))
+        return nums,
 
-    # A bunch of functions, that parse the problem number stdin to the thing that we actually shove into Solution.functhing(param1, ...)
-    # Assume the 
-    def parse_problem_119(stdin: str) -> int:
-        ## Each input is a string of 1,2,3,4,... We want to return a list of integers.
+    def strintconverter(stdin: str) -> int:
         return int(stdin.strip())
     
-
-    def parse_problem_287(stdin: str) -> List[int]:
-        # Takes stuff -> [1,2,3], ...
+    def lstrconverter(stdin: str) -> List[str]:
         words = list(map(int, stdin.strip().split(',')))
         return words,
 
-    def parse_problem_557(stdin: str) -> str:
+    def strstrconverter(stdin: str) -> str:
         return stdin
-        
-    def parse_problem_1048(stdin: str) -> List[str]:
-        # Each line of txt is string with list of words, convert to list[string]
-        words = list(map(str, stdin.strip().split(',')))
-        return words,
-
-    def parse_problem_1207(stdin: str) -> List[int]:
-        # Each line of txt is string with list of int, convert to list[int]
-        nums = list(map(int, stdin.strip().split(',')))
-        return nums,
