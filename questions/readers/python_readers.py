a0d798e674eda8f592b31620da151baa2ce148ef
Anti <spiralknightsowner@gmail.com>
2024-08-17 11:27:07 +1000

    Poggers

--- questions/readers/python_readers.py
+++ questions/readers/python_readers.py
@@ -21,19 +21,25 @@ class Problems:
 
     # A bunch of functions, that parse the problem number stdin to the thing that we actually shove into Solution.functhing(param1, ...)
     # Assume the 
-    def parse_probem_119(stdin: str) -> int:
+    def parse_problem_119(stdin: str) -> int:
         ## Each input is a string of 1,2,3,4,... We want to return a list of integers.
         return int(stdin.strip())
     
 
-    def parse_probem_287(stdin: str) -> List[int]:
+    def parse_problem_287(stdin: str) -> List[int]:
+        # Takes stuff -> [1,2,3], ...
+        words = list(map(int, stdin.strip().split(',')))
+        return words,
+
+    def parse_problem_557(stdin: str) -> str:
+        return stdin
         
-    def parse_probem_1048(stdin: str) -> List[str]:
+    def parse_problem_1048(stdin: str) -> List[str]:
         # Each line of txt is string with list of words, convert to list[string]
         words = list(map(str, stdin.strip().split(',')))
         return words,
 
-    def parse_probem_1207(stdin: str) -> List[int]:
+    def parse_problem_1207(stdin: str) -> List[int]:
         # Each line of txt is string with list of int, convert to list[int]
         nums = list(map(int, stdin.strip().split(',')))
         return nums,
