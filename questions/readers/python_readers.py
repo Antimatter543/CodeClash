edebdfd42fce870e3daff06d09705b64f6358250
Theodore Kim <tkim0476@gmail.com>
2024-08-17 15:12:48 +1000

    python reader is flexible now

--- questions/readers/python_readers.py
+++ questions/readers/python_readers.py
@@ -4,11 +4,11 @@ from typing import List
 class Problems:
     def lintconverter(self, stdin: str) -> List[int]:
         nums = list(map(int, stdin.strip().split(',')))
-        return nums,
+        return nums
     
     def lstrconverter(self, stdin: str) -> List[str]:
         words = list(map(str, stdin.strip().split(',')))
-        return words,
+        return words
 
     def intconverter(self, stdin: str) -> int:
         return int(stdin.strip())
--- questions/testing_scrapyard/test_judge.py
+++ questions/testing_scrapyard/test_judge.py
@@ -18,8 +18,8 @@ def submit_code_to_judge0(source_code: str, input_data: str) -> dict:
     return response.json()
 
 
-dictthing = {("Python", "listint"): "Problems().lintconverter", ("Python, liststr"): "Problems().lstrconverter",
-             ("Python", "int"): "Problems().intconverter", ("Python, str"): "Problems().strconverter",
+dictthing = {("Python", "listint"): "Problems().lintconverter", ("Python", "liststr"): "Problems().lstrconverter",
+             ("Python", "int"): "Problems().intconverter", ("Python", "str"): "Problems().strconverter",
 
              ("Java", "lintconverter"): "JavaProblems.lintconverter",("Java", "liststr"): "JavaProblems.lstrconverter",
              ("Java", "int"): "JavaProblems.intconverter",("Java", "str"): "JavaProblems.strconverter",
@@ -27,34 +27,83 @@ dictthing = {("Python", "listint"): "Problems().lintconverter", ("Python, listst
              ("C", "lintconverter"): "CProblems.lintconverter",("C", "liststr"): "CProblems.lstrconverter",
              ("C", "int"): "CProblems.intconverter",("C", "str"): "CProblems.strconverter",}
 
-dictquestion = {"76": "minWindow", "119": "getRow", "287": "findDuplicate", "557": "reverseWords",
-                "1048": "longestStrChain", "1207": "uniqueOccurrences"}
+dictquestion = {"119": ("getRow", "int"), "287": ("findDuplicate", "listint"), "557": ("reverseWords", "str"),
+                "1048": ("longestStrChain", "liststr"), "1207": ("uniqueOccurrences", "listint")}
 
 ## Given by frontend peeps
 language_used = "Python"
-user_code = """
-from typing import List
-class Solution:
-    Memo = {}
+# user_code = """
+# from typing import List
+# class Solution:
+#     Memo = {}
+#     def getRow(self, rowIndex: int) -> List[int]:
+#         if rowIndex in self.Memo:
+#             return self.Memo[rowIndex]
+#         if rowIndex == 0:
+#             return [1]
+#         ListPrec = self.getRow(rowIndex - 1)
+#         Result = [1]
+#         for i in range(0, len(ListPrec) - 1):
+#             Result.append(ListPrec[i] + ListPrec[i + 1])
+#         Result.append(1)
+#         self.Memo[rowIndex] = Result
+#         return Result
+# """ 
+# user_code = """
+# from typing import List
 
-    def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex in self.Memo:
-            return self.Memo[rowIndex]
-        if rowIndex == 0:
-            return [1]
-        ListPrec = self.getRow(rowIndex - 1)
-        Result = [1]
-        for i in range(0, len(ListPrec) - 1):
-            Result.append(ListPrec[i] + ListPrec[i + 1])
-        Result.append(1)
-        self.Memo[rowIndex] = Result
-        return Result
-""" 
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         slow, fast = 0, 0
+#         while True:
+#             slow = nums[slow]
+#             fast = nums[nums[fast]]
+#             if slow == fast:
+#                 break
+
+#         slow2 = 0
+#         while True:
+#             slow = nums[slow]
+#             slow2 = nums[slow2]
+#             if slow == slow2:
+#                 return slow
+# """
+
+# user_code = """
+# class Solution:
+#     def reverseWords(self, s):
+#         return " ".join(word[::-1] for word in s.split(" "))
+# """
+
+# user_code = """
+# class Solution:
+#     def longestStrChain(self, words):
+#         words.sort(key = len)
+#         dp = {}
+#         for word in words:
+#             dp[word] = 1
+#             for i in range(len(word)):
+#                 prev = word[:i] + word[i + 1:]
+#                 if prev in dp:
+#                     dp[word] = max(dp[word], dp[prev] + 1)
+#         return max(dp.values())
+# """
+
+user_code = """
+class Solution:
+    def uniqueOccurrences(self, arr):
+        from collections import Counter
+        cnts = Counter(arr).values()
+        return len(cnts) == len(set(cnts))
+"""
+# Front End gives question number
+question = "1207"
+question_type = dictquestion[question][1]
 # Specify the path to your file
 file_path = './questions/readers/python_readers.py'
 
-# hard coding the type of question
-question = "119"
+
+
 with open(file_path, 'r') as file:
     file_contents = file.read()
 # print(file_contents)
@@ -66,11 +115,10 @@ if language_used == "Python":
 {file_contents} \n
     
 inputs = input()
-parsed_input = {dictthing[(language_used, "int")]}(inputs)
-
+parsed_input = {dictthing[(language_used, question_type)]}(inputs)
 
 sol = Solution()
-print(sol.{dictquestion[question]}(parsed_input))
+print(sol.{dictquestion[question][0]}(parsed_input))
 """
 
 # print(judge_code)
@@ -87,7 +135,7 @@ def read_input_file(file_path: str) -> list:
 input_data = read_input_file('./questions/files/'+ question +'_test_cases.txt')
 expected_output = read_input_file('./questions/files/'+ question +'_answers.txt')
 
-print(input_data, expected_output)
+# print(input_data, expected_output)
 
 ### Throw it to judge, and handle the result
 
