3f23418fcae13ccbb93dec8854ceb33d9d86cdbf
Anti <spiralknightsowner@gmail.com>
2024-08-17 12:33:16 +1000

    Added simple java reader thing and also scrapyard to be deleted eventually

--- questions/readers/java_reader.java
+++ questions/readers/java_reader.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class JavaProblems {
 
-class JavaProblems
+    public static List<Integer> lintconverter(String stdin) {
+        List<Integer> nums = new ArrayList<>();
+        for (String numStr : stdin.split(",")) {
+            nums.add(Integer.parseInt(numStr.trim()));
+        }
+        return nums;
+    }
+
+    public static List<String> lstrconverter(String stdin) {
+        List<String> words = new ArrayList<>();
+        words.addAll(Arrays.asList(stdin.split(",")));
+        return words;
+    }
+
+    public static int intconverter(String stdin) {
+        return Integer.parseInt(stdin.trim());
+    }
+
+    public static String strconverter(String stdin) {
+        return stdin.trim();
+    }
+}
