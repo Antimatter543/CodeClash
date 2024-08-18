e611fd1cc7de39aa432970c900d0db34ac51e1a0
Anti <spiralknightsowner@gmail.com>
2024-08-18 11:36:16 +1000

    Update constants.py

--- aws_backend/constants.py
+++ aws_backend/constants.py
@@ -1,3 +1,3 @@
-EC2_IP = "13.211.214.175" # public ip of ec2 instance
+EC2_IP = "54.253.188.5" # public ip of ec2 instance
 
 JUDGE0_API_URL = f"http://{EC2_IP}:2358/submissions/?base64_encoded=false&wait=true"
